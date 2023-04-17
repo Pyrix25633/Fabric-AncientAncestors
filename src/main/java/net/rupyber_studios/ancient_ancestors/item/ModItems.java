2fee5bb5791bcadea3e9b035f5dee7080d4b98fa
Pyrix25633 <biral.mattia@gmail.com>
2023-04-17 14:35:16 +0200

    Added Ancient Clay Ball and Ancient Brick with craftings

--- src/main/java/net/rupyber_studios/ancient_ancestors/item/ModItems.java
+++ src/main/java/net/rupyber_studios/ancient_ancestors/item/ModItems.java
@@ -1,11 +1,19 @@
 package net.rupyber_studios.ancient_ancestors.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.rupyber_studios.ancient_ancestors.AncientAncestors;
 
 public class ModItems {
+    private static final FabricItemSettings itemSettings = new FabricItemSettings().group(ModItemGroup.ANCIENT_ANCESTORS);
+
+    public static final Item ANCIENT_CLAY_BALL = registerItem("ancient_clay_ball",
+            new Item(itemSettings));
+    public static final Item ANCIENT_BRICK = registerItem("ancient_brick",
+            new Item(itemSettings));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(AncientAncestors.MOD_ID, name), item);
     }
